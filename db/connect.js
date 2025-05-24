714e81cb53f6a06ea8c168c0ab4f01de076e5803
RLTB-Rommel <rommelaunario101@gmail.com>
2025-05-25 04:18:50 +0800

    commit

--- db/connect.js
+++ db/connect.js
@@ -1,22 +1,24 @@
-require('dotenv').config();
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
 let db;
 
 async function connectToDatabase() {
-  const client = new MongoClient(process.env.MONGO_URI);
+  const client = new MongoClient(process.env.MONGO_URI, {
+    tls: true, // Explicitly enable TLS
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: { version: '1', strict: true, deprecationErrors: true },
+  });
+
   await client.connect();
-  db = client.db('cse341'); // You can name the DB anything
+  db = client.db('cse341');
   console.log('Connected to MongoDB');
 }
 
 function getDb() {
-  if (!db) {
-    throw new Error('Database not connected!');
-  }
+  if (!db) throw new Error('Database not connected!');
   return db;
 }
 
 module.exports = { connectToDatabase, getDb };
-console.log('MONGO_URI:', process.env.MONGO_URI);
