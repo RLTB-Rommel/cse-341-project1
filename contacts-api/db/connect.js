1a88d3f26820852e77d7aa3264d2919a6fe45801
RLTB-Rommel <rommelaunario101@gmail.com>
2025-05-25 20:18:30 +0800

    move

---
+++ contacts-api/db/connect.js
@@ -0,0 +1,22 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+let db;
+
+async function connectToDatabase() {
+    const client = new MongoClient(process.env.MONGO_URI, {
+      tls: true,
+      serverApi: { version: '1', strict: true, deprecationErrors: true },
+    });
+
+  await client.connect();
+  db = client.db('cse341');
+  console.log('Connected to MongoDB');
+}
+
+function getDb() {
+  if (!db) throw new Error('Database not connected!');
+  return db;
+}
+
+module.exports = { connectToDatabase, getDb };
