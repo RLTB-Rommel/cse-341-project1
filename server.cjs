b2845a250954b316757322a8787cdecb5f312d06
RLTB-Rommel <rommelaunario101@gmail.com>
2025-05-25 19:37:26 +0800

    new_task

--- server.cjs
+++ server.cjs
@@ -1,17 +1,35 @@
 const express = require('express');
 const app = express();
 const { connectToDatabase } = require('./db/connect');
-const routes = require('./routes'); // automatically loads routes/index.js
+const routes = require('./routes'); // loads routes/index.js
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 require('dotenv').config();
+
 const PORT = process.env.PORT || 3000;
+
+// Swagger setup
+const swaggerSpec = swaggerJsdoc({
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Contacts API',
+      version: '1.0.0',
+      description: 'API documentation for contacts',
+    },
+  },
+  apis: ['./routes/*.cjs'], // pulls JSDoc from your contacts.cjs file
+});
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Middleware
 app.use(express.json());
 
-// Use all routes from routes/index.js
+// Load all app routes
 app.use('/', routes);
 
+// Connect to MongoDB and start server
 connectToDatabase()
   .then(() => {
     app.listen(PORT, () => {
