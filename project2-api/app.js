4b794892db0d4749ebb95e02478d57dd848761b4
RLTB-Rommel <rommelaunario101@gmail.com>
2025-06-01 05:35:21 +0800

    cors

--- project2-api/app.js
+++ project2-api/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const { connectToDatabase } = require('./db/connect');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swagger/swagger'); 
@@ -6,6 +7,7 @@ const itemRoutes = require('./routes/items');
 const userRoutes = require('./routes/users');  
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
