53461568a0dc5adf95a8ab17f4189d84123ef579
RLTB-Rommel <rommelaunario101@gmail.com>
2025-06-01 07:07:44 +0800

    swagger_authorize

--- project2-api/swagger/swagger.js
+++ project2-api/swagger/swagger.js
@@ -1,4 +1,3 @@
-// swagger/swagger.js
 const swaggerJsdoc = require('swagger-jsdoc');
 
 const options = {
@@ -7,15 +6,29 @@ const options = {
     info: {
       title: 'Project 2 API',
       version: '1.0.0',
-      description: 'CRUD API with MongoDB',
+      description: 'CRUD API with MongoDB and JWT authentication',
     },
     servers: [
       {
-        url: 'http://localhost:3000', // Change to your Render link after deployment
+        url: 'http://localhost:3000', // or your Render URL after deploy
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
       },
     ],
   },
-  apis: ['./routes/*.js'], // Path to the route files for auto-generated docs
+  apis: ['./routes/*.js'], // Path to route files for Swagger comments
 };
 
 const swaggerSpec = swaggerJsdoc(options);
