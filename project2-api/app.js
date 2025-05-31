c3a17d354afd81883d6eba02a594254f152e9054
RLTB-Rommel <rommelaunario101@gmail.com>
2025-06-01 06:41:42 +0800

    Add JWT_SECRET and prepare for authentication

--- project2-api/app.js
+++ project2-api/app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const { connectToDatabase } = require('./db/connect');
 const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./swagger/swagger'); 
-const itemRoutes = require('./routes/items'); 
-const userRoutes = require('./routes/users');  
+const swaggerSpec = require('./swagger/swagger');
+
+const itemRoutes = require('./routes/items');
+const userRoutes = require('./routes/users');
+const authRoutes = require('./routes/auth'); 
+const verifyToken = require('./middleware/verifyToken');
+
 const app = express();
 
 app.use(cors());
@@ -12,9 +16,12 @@ app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// Mount your routes
-app.use('/api/items', itemRoutes);
-app.use('/api/users', userRoutes); 
+// Auth route (no token needed)
+app.use('/api/auth', authRoutes);
+
+// Protected routes (token required)
+app.use('/api/items', verifyToken, itemRoutes);
+app.use('/api/users', verifyToken, userRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running');
